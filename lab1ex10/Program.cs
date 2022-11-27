1f405185f142a02db534b5d35905b633906b5381
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 20:31:38 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace lab1ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 10
+                    Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la tastatura.
+                     Numarul are exact 2 cifre
+             */
+
+            Console.WriteLine("num = ");
+            string num = Console.ReadLine();
+
+            int result = num.Sum(p => int.Parse(p.ToString()));
+
+            Console.WriteLine($"Suma cifrelor {num} este {result}");
+        }
+    }
+}
